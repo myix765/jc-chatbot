4d5e6e8b5fd44df639c203a4b736851a5335df1f
myix765 <myix765@gmail.com>
2024-07-26 22:11:47 -0400

    styled user query and ai response

--- client/src/components/MessageBlock.jsx
+++ client/src/components/MessageBlock.jsx
@@ -1,8 +1,15 @@
 import PropTypes from 'prop-types'
 
-const MessageBlock = ({ message }) => {
+const MessageBlock = ({ message, isQuery }) => {
+    let styleModifier;
+    if (isQuery) {
+        styleModifier = "self-end border-black"
+    } else {
+        styleModifier = "border-green-300"
+    }
+
     return (
-        <div className="border-2 rounded-lg border-black p-1">
+        <div className={`border-2 rounded-lg p-1 w-[70%] ${styleModifier}`}>
             <p>{message}</p>
         </div>
     )
@@ -10,6 +17,7 @@ const MessageBlock = ({ message }) => {
 
 MessageBlock.propTypes = {
     message: PropTypes.string.isRequired,
+    isQuery: PropTypes.bool.isRequired
 }
 
 export default MessageBlock;
--- client/src/components/MessageList.jsx
+++ client/src/components/MessageList.jsx
@@ -4,11 +4,22 @@ import PropTypes from 'prop-types'
 const MessageList = ({ messageArr }) => {
     return (
         <div className="flex flex-col gap-2 w-full">
-            {messageArr.map((messageObj) => {
-                return <MessageBlock
-                    key={messageObj.id}
-                    message={messageObj.query}
-                />
+            {messageArr.map((messageObj, id) => {
+                console.log("messageObj:", messageObj);
+                return (
+                    <>
+                        <MessageBlock
+                            // key={id}
+                            message={messageObj.query}
+                            isQuery={true}
+                        />
+                        <MessageBlock
+                            // key={id}
+                            message={messageObj.response}
+                            isQuery={false}
+                        />
+                    </>
+                )
             })}
         </div>
     )
