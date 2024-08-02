fb09f000465c7dfd44abd8a7a225d050ca12853d
myix765 <myix765@gmail.com>
2024-08-02 00:16:25 -0400

    change pool to use vercel postgres connection string

--- server/db_pool.js
+++ server/db_pool.js
@@ -1,12 +1,16 @@
 const Pool = require('pg').Pool;
 require('dotenv').config;
 
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.HOST,
+//     database: process.env.DB,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.HOST,
-    database: process.env.DB,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+    connectionString: process.env.POSTGRES_URL,
+})
 
 module.exports = pool;
