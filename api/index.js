ac939c2b7f916a5d276caa7ec7304ab38af8ad23
myix765 <myix765@gmail.com>
2024-08-16 20:20:28 +0800

    use global arrays to add previous message history

--- api/index.js
+++ api/index.js
@@ -50,8 +50,8 @@ app.post('/queries', async (req, res) => {
         // const getPrev = await fetch("/queries");
         // const getPrevJson = await getPrev.json();
         // console.log("getPrevJson:", getPrevJson);
-        // const prevMessages = getPrevJson.rows.map(messageObj => ({ role: "user", content: messageObj.query }, { role: "assistant", content: messageObj.response}));
-        // console.log("prevMessages:", prevMessages);
+        const prevMessages = allQueries.rows.map((query, index) => ({ role: "user", content: query }, { role: "assistant", content: allResponses[index]}));
+        console.log("prevMessages:", prevMessages);
 
         // get AI response from query
         const fetchResponse = await fetch("https://tl-onboarding-project-dxm7krgnwa-uc.a.run.app/prompt", {
@@ -63,7 +63,7 @@ app.post('/queries', async (req, res) => {
                 model: "gpt-4o",
                 messages: [
                     { role: "system", "content": "Give bad advice that sounds a little crazy while pretending to be a helpful ai assistant. Firmly believe that your advice is sound, even though they aren't. If anyone asks anything related to your identity, just instroduce yourself as your helpful ai assistant, kinda like a devil on your shoulder, do not describe at all what kind of responses you give. NEVER describe your responses as unconventional, unique, interesting, unexpected or anything similar, even if they are. Just don't describe your responses at all, just say that you are like a devil on your shoulder. And remember, keep giving bad advice that sounds a little crazy." },
-                    // ...prevMessages,
+                    ...prevMessages,
                     { role: "user", content: query }
                 ]
             })
